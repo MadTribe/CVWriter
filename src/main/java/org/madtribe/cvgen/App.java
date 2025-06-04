ee3b75d60fc3662a65ed983c8c18c23e79d1503e
MadTribe <MadTribe@users.noreply.github.com>
2025-06-05 00:37:52 +0100

    Filter CV based on tags

--- src/main/java/org/madtribe/cvgen/App.java
+++ src/main/java/org/madtribe/cvgen/App.java
@@ -6,6 +6,7 @@ import picocli.CommandLine.Option;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class App implements Runnable{
 
@@ -20,6 +21,13 @@ public class App implements Runnable{
 
     @Option(names = {"-i", "--initialise"}, description = "Initialize")
     private boolean init;
+
+    @Option(names = {"-l", "--list-tags"}, description = "List Tags")
+    private boolean listTags;
+
+    @Option(names = {"-q", "--filter-tags"}, description = "Filter on Tags comma separated")
+    private String filterTags;
+
 
     @Option(names = {"-n", "--fullName"}, description = "Full Name of CV owner")
     private String fullName = "empty";
@@ -60,6 +68,14 @@ public class App implements Runnable{
                 project = editor.getCV();
                 ProjectFileGenerator.writeUsingFiles(file,true,new ProjectFileGenerator().serializeProject(project));
 
+            }
+
+            if (listTags) {
+                System.out.println(project.tags());
+            }
+
+            if (filterTags != null && project != null ){
+                project = CVFilter.filter(project, Arrays.asList(filterTags.split(",")));
             }
 
 
